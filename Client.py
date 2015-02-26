e217177cd44e1f4f5f47d25752f5c84de6f2ab3e
fhausken <fredrik280893@gmail.com>
2015-02-26 14:31:50 +0100

    oh yeah pussies

--- Client.py
+++ Client.py
@@ -30,11 +30,10 @@ class Client:
 
     def receive_message(self, message):
         # TODO: Handle incoming message
-        pass
+        self.connection
 
-    def send_payload(self, data):
-        # TODO: Handle sending of a payload
-        pass
+    def send_payload(self, client, data):
+        self.connection.send("" + client + data)
 
 
 if __name__ == '__main__':
@@ -42,7 +41,8 @@ if __name__ == '__main__':
     This is the main method and is executed when you type "python Client.py"
     in your terminal.
 
-    No alterations is necessary
+    No alterations is necessary.'localhost' 
     """
-    client = Client('localhost', 9998)
+    client = Client('78.91.42.106', 9998)
     client.run()
+    client.send_payload(1,'Hei')
