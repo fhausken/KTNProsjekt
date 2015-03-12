55f9dfbbde065bae2fb518d526247daa9cf9b328
MaSommer <martisom@stud.ntnu.no>
2015-03-12 14:52:04 +0100

    kok

--- Client.py
+++ Client.py
@@ -1,68 +1,18 @@
 # -*- coding: utf-8 -*-
+'''
+KTN-project 2015 - Squeak Chat
+'''
 import socket
 import json
 import threading
 import re
-import MessageReceiver
 
-class Client:
-    """
-    This is the chat client class
-    """
+class Client(object):
 
-    def __init__(self, host, server_port):
-        """
-        This method is run when creating a new Client object
-        """
-        # Set up the socket connection to the server
+    def __init__(self):
         self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        print "Welcome to the bigtime chat!\nPlease specify server ip:port, or leave empty for the defaults "+host+":"+str(server_port)
-        innInfo=raw_input('>')
-        if innInfo :
-            host=innInfo.split(":")[0]
-            port=int(innInfo.split(":")[1])
-            
-        self.connection.connect((host, port))
-        self.logged_in = False
-        self.commands = {"/logout":self.disconnect}
-        
-        while not self.logged_in:
-            self.username = raw_input('Username: ')
-            self.send(self.parse({'request':'login', 'username':self.username}))
-            response = self.connection.recv(1024).strip()
-            self.process_json(response)
-        
-        #messagerece = MessageReceiver();
-        #messagerece.__init__(self, )
-        
-        while self.logged_in:
-            received_data = self.connection.recv(1024).strip()
-            self.process_json(received_data)
-        self.connection.close()
-        
-        self.host=host
-        self.server_port=server_port
-
-    def run(self):
-        # Initiate the connection to the server
-        # hello 
-        self.connection.connect((self.host, self.server_port))
-        print("Connected")
-
-    def disconnect(self):
-        self.connection.close()
-
-    def receive_message(self, message):
-        # TODO: Handle incoming message
-        self.connection
-
-    def send_payload(self, data):
-        self.connection.send("" + data)
     
-    def parse(self, data):
-        return json.dumps(data)
-    
-    def process_json(self, data):
+    def process_json(self, data): #konverterer fra json til normal form
         index = 0
         while data.find("{", index) >= 0:
             start = data.find("{", index)
@@ -70,24 +20,81 @@ class Client:
             index = end
             self.process_data(data[start:end+1])
     
-    
+    def process_data(self, data):
+        decoded = json.loads(data)
+        if decoded.get("response", "") == "login":
+            if decoded.get("error", "") != "":
+                print decoded["error"], "(%s)"%decoded.get("username", "")
+            else:
+                self.logged_in = True
+            if decoded.get("messages", "") != "":
+                print decoded["messages"].encode('utf-8')
 
-if __name__ == '__main__':
-    """
-    This is the main method and is executed when you type "python Client.py"
-    in your terminal.
-
-    No alterations is necessary.'localhost' 
-    """
-    client = Client('localhost', 9998)
-    client.run()
-    client.send_payload('Hei')
-    try:
-        client.send_payload('Fredrik: ',"Hei")
-        client.send_payload('Fredrik: ',"Jeg er keen på is")
-    finally:
+        if not self.logged_in:
+            return
         
-        client.disconnect()
-        print("Socket is shutdown")
+        if decoded.get("response", "") == "logout":
+            self.logged_in = False
+
+        if decoded.get("response", "") == "message":
+            print decoded["message"].encode('utf-8')
+                
+
+    def start(self, host, port):
+       # self.__init__()
+        print "Welcome to this groupchat!\nPlease specify server ip:port, or leave blank for the defaults "+host+":"+str(port)
+        innInfo=raw_input('>')
+        if innInfo :
+            host=innInfo.split(":")[0]
+            port=int(innInfo.split(":")[1])
+            
+        self.connection.connect((host, port))
+        self.logged_in = False  
+        self.commands = {"/logout":self.disconnect}
+        
+        while not self.logged_in:
+            self.username = raw_input('Username: ')
+            self.login()
+            response = self.connection.recv(1024).strip()
+            self.process_json(response)
+        
+        #make take_input work in a thread.
+        t = threading.Thread(target=self.take_input)
+        t.setDaemon=True
+        t.start()
+        
+        while self.logged_in:
+            received_data = self.connection.recv(1024).strip()
+            self.process_json(received_data)
+        self.connection.close()
+
+
+
+    def send(self, data):
+        self.connection.sendall(data)
+        
+    def login(self):
+        self.send(self.parse({'request':'login', 'username':self.username}))
     
+    def disconnect(self):
+        self.send(self.parse({'request':'logout'}))
+        
+    def parse(self, data):
+        return json.dumps(data)
     
+    def take_input(self):
+        """lets the user write a message"""
+        while True:
+            #this thread stops here until it has data, so no need for time.sleep
+            data = raw_input()
+            command = re.findall("^[/]\w+", data)
+            if command:
+                if command[0] in self.commands:
+                    self.commands[command[0]]()
+                continue
+            if data != "":
+                self.send(self.parse({"request":"message", "message":data}))
+
+if __name__ == "__main__":
+    client = Client()
+    client.start('localhost', 9999)
--- Server.py
+++ Server.py
@@ -1,39 +1,67 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime
 import json
 import threading
 import time
 import SocketServer
 import re
 
-messages = []
-usernames = []
+#global variable that holds all the messages being sent by all the clients
+#This needs to be global to be able to share some data between all the ClientHandler threads.
+#only read from it or append to it.
+#whatever you append to all_messages will be broadcasted to all users.
+all_messages = []
+users = []
 
 class ClientHandler(SocketServer.BaseRequestHandler):
-    """
-    This is the ClientHandler class. Everytime a new client connects to the
-    server, a new ClientHandler object will be created. This class represents
-    only connected clients, and not the server itself. If you want to write
-    logic for the server, you must write it outside this class
-    """
-    global messages
-    global usernames
+    global all_messages
+    global users
     
-    def handle(self):
-        """
-        This method handles the connection between a client and the server.
-        """
+    def timestamp(self):
+        return datetime.now().strftime("%H:%M")
+    
+    def broadcast(self, data):
+        """sends data to all clients"""
+        all_messages.append(data)
+    
+    #This function must do all the work required to service a request. The default implementation does nothing. 
+    #Several instance attributes are available to it; the request is available as self.request; the client address as self.client_address;
+    #and the server instance as self.server, in case it needs access to per-server information.
+    def handle(self): #denne kalles når superklassen instansieres da handle blir kallt i superkonstruktøren
+        # self.request is the TCP socket connected to the client
+        self.connection = self.request
         self.ip = self.client_address[0]
         self.port = self.client_address[1]
-        self.connection = self.request
-
-        # Loop that listens for messages from the client
+        self.logged_in = False
+        self.sentdata = 0
+            
+        #Logg clienter som prÃ¸ver Ã¥ koble til
+        print 'Client connected @' + self.ip + ':' + str(self.port)
+                    
+        #make send_updates to work in a thread.
+        self.t = threading.Thread(target=self.send_updates)
+        self.t.setDaemon=True
+        self.t.start()
+        
         while True:
-            received_string = self.connection.recv(4096)
-            print(received_string)
-            #TODO: Add handling of received payload from client
-            print(received_string)
-     
-         
+            #wait here for data
+            data = self.connection.recv(1024).strip()
+            # Check if the data exists
+            # (recv could have returned due to a disconnect)
+            if data:
+                print(data)
+                self.process_data(data)
+                #print data
+            else:
+                print ('Client disconnected!')
+                break   
+        
+    def finish(self):
+        try:
+            users.remove(self.username)
+        except:
+            pass
+        
     def process_data(self, data):
         decoded = json.loads(data)
         
@@ -48,49 +76,55 @@ class ClientHandler(SocketServer.BaseRequestHandler):
           
         if decoded['request'] == 'message':
             if decoded.get("message", "") != "":
-                padd=" "*(len(max(usernames, key=len))-len(self.username))
+                padd=" "*(len(max(users, key=len))-len(self.username))
                 message = self.timestamp()+padd+" %s| %s"%(self.username, decoded['message'])
                 self.broadcast(message)
     
-         
+
     def login(self, username):
         if(not re.match(r'^[A-Za-z0-9_]+$', username)):
             self.send({'response':'login', 'error':'Invalid username!', 'username':username}) 
             return
-        if not username in usernames:
+        if not username in users:
             self.username = username
-            usernames.append(username)
+            users.append(username)
             self.logged_in = True
-            self.request.sendall({'response':'login', 'username':self.username})
-            messages.append("*** " + self.username + " has joined the chat.")
+            self.send({'response':'login', 'username':self.username})
+            self.broadcast("*** " + self.username + " has joined the chat.")
 
         else:
             self.send({'response': 'login', 'error':'Name already taken!', 'username':username})
-            
+        
+    def logout(self):
+        try:
+            users.remove(self.username)
+            self.logged_in = False
+            self.send({'response': 'logout', 'nick': self.username})
+            self.broadcast("*** " + self.username + " has left the chat.")
+        except ValueError:
+            self.send({'response': 'logout', 'error':'Not logged in!', 'nick': self.username})
     
-class ThreadedTCPServer(SocketServer.ThreadingMixIn, SocketServer.TCPServer):
-    """
-    This class is present so that each client connected will be ran as a own
-    thread. In that way, all clients will be served by the server.
+    def send(self, data):
+        self.request.sendall(json.dumps(data))
+    
+    def send_updates(self):
+        while True:
+            if self.sentdata < len(all_messages) and self.logged_in:
+                    #couldnt send a list directly, will fix with json later
+                    for x in range(self.sentdata, len(all_messages)):
+                        self.send({"response":"message", "message":all_messages[x]})
+                        self.sentdata += 1
+            #Have to sleep it, else it will try to drain the cpu
+            time.sleep(0.2) #0.2 seconds
 
-    No alterations is necessary
-    """
-    allow_reuse_address = True #Statisk variabel
-
+# KjÃ¸res nÃ¥r programmet startes
 if __name__ == "__main__":
-    """
-    This is the main method and is executed when you type "python Server.py"
-    in your terminal.
+    # Definer host og port for serveren
+    HOST = '0.0.0.0'
+    PORT = 9996
 
-    No alterations is necessary
-    """
-    HOST, PORT = '0.0.0.0', 9998
-    print 'Server running...'
-    
-    #Set up and initiate the TCP server
-    server = ThreadedTCPServer((HOST, PORT), ClientHandler)
+    # Sett opp serveren
+    server = SocketServer.TCPServer((HOST, PORT), ClientHandler)
+
+    # Aktiver serveren. Den vil kjÃ¸re til den avsluttes med Ctrl+C
     server.serve_forever()
-    
- 
-
-
